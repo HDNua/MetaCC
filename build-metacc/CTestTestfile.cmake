3b8384412cf23f77243911c1e2d51d66e21fb5e8
Doyoung Han <rbfwmqwntm@naver.com>
2020-12-06 18:00:45 +0900

    merging4

---
+++ build-metacc/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/handy/Dropbox/linux/work/yacc/metacc/latest/metacc
+# Build directory: /home/handy/Dropbox/linux/work/yacc/metacc/latest/build-metacc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(basic "/home/handy/Dropbox/linux/work/yacc/metacc/latest/metacc/test/test0.pl")
+set_tests_properties(basic PROPERTIES  WORKING_DIRECTORY "/home/handy/Dropbox/linux/work/yacc/metacc/latest/build-metacc" _BACKTRACE_TRIPLES "/home/handy/Dropbox/linux/work/yacc/metacc/latest/metacc/CMakeLists.txt;38;add_test;/home/handy/Dropbox/linux/work/yacc/metacc/latest/metacc/CMakeLists.txt;0;")
